ec7d14ef1cc20bd7815644d877c30834bf5eade5
Yoon Gwan <blackgar@naver.com>
2022-12-02 06:48:51 +0900

    방에 참여하기 + 방에 누군가 참여하면 모두에게 메시지 보내기 + 화면 구현

--- SocketIO활용하기/src/public/js/app.js
+++ SocketIO활용하기/src/public/js/app.js
@@ -2,16 +2,44 @@
 const socket = io();
 
 const $welcome = document.getElementById("welcome");
+const $room = document.getElementById("room");
 const $form = $welcome.querySelector("form");
+
+// 처음에는 방을 보여주지 않고 숨겼다가 방에 들어가면 방을 보여주도록 한다.
+$room.hidden = true;
+
+let roomName;
+
+// 인자를 받아 메시지를 추가해주는 함수
+function addMessage(message) {
+  const $ul = $room.querySelector("ul");
+  const $li = document.createElement("li");
+  $li.innerText = message;
+  $ul.appendChild($li);
+}
+
+// 백엔드에서 처리 완료했을 때 작동시킬 함수, 서버측에서 인자 전달이 가능하다.
+function showRoom() {
+  // 방에 들어가게 되면 방이름 입력창을 안보이게 하고 방 안에 채팅 부분을 보이게 하기
+  $welcome.hidden = true;
+  $room.hidden = false;
+  const h3 = $room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
+}
 
 function handleRoomSubmit(event) {
   event.preventDefault();
   const $input = $form.querySelector("input");
-  // socket.io의 경우 send가 아닌 emit으로 메시지를 전달하는데 메시지만 보내는 것이 아니라 첫 번째 인자로 특정 이벤트("enter_room")를 지정해서 보내고 두 번째 인자로 전달할 메시지 내용을 넘겨준다. 이 때 object타입도 그냥 전달할 수 있다. 그리고 세 번째 인자로 server에서 실행할 callback함수를 지정해서 전달할 수 있다.
-  socket.emit("enter_room", { payload: $input.value }, () => {
-    console.log("서버에서 해당 메시지를 받아 작업 수행을 완료했습니다.");
-  });
+  // socket.io의 경우 send가 아닌 emit으로 메시지를 전달하는데 메시지만 보내는 것이 아니라 첫 번째 인자로 특정 이벤트("enter_room")를 지정해서 보내고 두 번째 인자로 전달할 메시지(내용)을 넘겨준다. 이 때 object타입을 포함한 다양한 타입의 메시지를 그냥 전달할 수 있다. 그리고 세 번째 인자로 server에서 프론트엔드에서 실행시킬 callback함수를 지정해서 전달할 수 있다.
+  // 이 때 중간에 하나의 값이 아닌 여러개의 값을 연달아 전달할 수 있다. 단, 함수를 포함시킬 때는 무조건 마지막에 넣어야 한다.
+  socket.emit("enter_room", $input.value, showRoom);
+  roomName = $input.value;
   $input.value = "";
 }
 
 $form.addEventListener("submit", handleRoomSubmit);
+
+// server에서 메시지를 보낼 때 내보낸 welcome event가 감지되면 메시지를 추가하는 함수 실행하기.
+socket.on("welcome", () => {
+  addMessage("Someone Joined");
+});
--- SocketIO활용하기/src/server.js
+++ SocketIO활용하기/src/server.js
@@ -18,17 +18,36 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
+  // 무슨 이벤트가 발생하건 실행시키는 코드
+  socket.onAny((event) => {
+    console.log(`Socket Event:${event}`);
+  });
   // sockets: Map(1) { 'nnv2FK4mdnNSErGYAAAB' => [Circular *1] } 내부적으로 자동으로 연결된 브라우저를 추적한다.
+  // 서버가 닫히더라도 socket.io는 계속해서 재연결을 시도한다.
+  // 다양한 정보들에 대해서는 socket.io docs 확인. 여러 메서드 포함
   // console.log(socket);
   // socket.io에서는 프론트엔드에서 전달한 이벤트를 감지해서 전달해주는 인자를 받아올 수 있다. 이 때 무조건 프론트엔드에서 지정한 이벤트 이름과 같은 이벤트로 받아야 한다.
   // 마찬가지로 두 번째로 전달된 메시지를 매개변수로 받아와서 활용할 수 있고 세 번째로 전달된 callback 함수를 바로 사용할 수 있다.
-  socket.on("enter_room", (msg, done) => {
-    console.log(msg);
-    setTimeout(() => {
-      // 서버 작업이 끝난 뒤 문구 표시를 위한 함수
-      // 브라우저 콘솔창에 나타나도록 한다.
-      done();
-    }, 1000);
+  socket.on("enter_room", (roomName, showRoom) => {
+    // 현재 socket의 id값을 보여준다.
+    // console.log(socket.id);
+    // console.log(roomName);
+    // 현재 어떤 소켓들이 있는지 보여준다.
+    // 참고로 user은 private room이 언제나 생성되기 때문에 기본적으로 유저 id로 이루어진 방이 있다.
+    // console.log(socket.rooms);
+    // 방을 생성해주는 방법
+    socket.join(roomName);
+    // 방에 들어가게 됐을 때 방에 대한 내용을 보여주는것으로 바꿔주는 함수
+    showRoom();
+    // roomName안에 있는 모든 유저들에게 메시지 보내기
+    socket.to(roomName).emit("welcome");
+    // console.log(socket.rooms);
+    // setTimeout(() => {
+    //   // 서버 작업이 끝난 뒤 완료 문구 표시를 위한 함수
+    //   // 브라우저 콘솔창에 나타나도록 한다.
+    //   // 인자도 전달이 가능하다.
+    //   done("처리했습니다.");
+    // }, 1000);
   });
 });
 
