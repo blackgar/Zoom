35ef6fff1054c7024f24e54fe70011396d543574
Yoon Gwan <blackgar@naver.com>
2022-12-02 01:58:35 +0900

    socket의 속성을 활용한 닉네임 설정과 메시지 구분 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,11 +1,16 @@
 const $ul = document.querySelector("ul");
-const $form = document.querySelector("form");
-
+const $nickname = document.querySelector("#nickname");
+const $message = document.querySelector("#message");
 // front-end와 back-end를 연결시켜주는 new WebSocket
 // 그냥 주소를 입력해도 되지만, 현재 어느 위치를 바라보고 있는지 변수명으로 지정해주면 나중에 휴대폰으로 접속해도 정상적인 url을 제공해줄 수 있다.
 // server 부분의 socket과 같은 변수명이라 헷갈릴 수 있는데 이 떄의 socket은 서버와의 연결을 의미하고 server에서의 socket은 연결된 브라우저를 의미한다.
 // front-end에서 backend와 연결하기 위해서는 ws주소를 통해 연결을 해야 한다.
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 // 서버와 연결이 됐을 때 open event를 감지해 함수 실행
 socket.addEventListener("open", () => {
@@ -14,7 +19,10 @@ socket.addEventListener("open", () => {
 
 // 서버에서 전달된 메시지를 message event를 감지해 읽어와서 보여주기
 socket.addEventListener("message", (message) => {
-  console.log("새로운 메시지 :", message.data);
+  // console.log("새로운 메시지 :", message.data);
+  const $li = document.createElement("li");
+  $li.innerText = message.data;
+  $ul.append($li);
 });
 
 // 서버가 오프라인이 될 때의 close event를 감지해 함수 실행
@@ -29,11 +37,23 @@ socket.addEventListener("close", () => {
 
 function handleSubmit(event) {
   event.preventDefault();
-  const $input = $form.querySelector("input");
+  const $input = $message.querySelector("input");
   console.log($input.value);
   // input에 입력된 값을 서버로 전달.
-  socket.send($input.value);
+  socket.send(makeMessage("message", $input.value));
+  $input.value = "";
+}
+// 이렇게 메시지를 두 군데서 보내게 되면 백엔드에서는 그냥 모두 똑같은 메시지로 인식해버리는 문제가 생긴다.
+// 그렇기 때문에 메시지를 서버로 보낼 때 그냥 text만 보내는 것이 아니라 json형태로 type을 지정해서 해당 type에 맞는 처리를 서버에서 할 수 있도록 보내줘야 한다.
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const $input = $nickname.querySelector("input");
+  console.log($input.value);
+  // input에 입력된 값을 서버로 전달.
+  // 서버에서는 string만 받을 수 있기 때문에 이러한 json 형태는 stringify해서 보낸다음 server에서 JSON.parse로 풀어서 사용한다.
+  socket.send(makeMessage("nickname", $input.value));
   $input.value = "";
 }
 
-$form.addEventListener("submit", handleSubmit);
+$message.addEventListener("submit", handleSubmit);
+$nickname.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import WebSocket from "ws";
 import express from "express";
+import { parse } from "path";
 
 const app = express();
 
@@ -26,8 +27,15 @@ const server = http.createServer(app);
 // 이렇게 wss에 server을 넣어주게 되면 http 위에 websocket을 생성해 한 서버에서 둘 다 사용가능. 즉, 포트 3000으로 websocket과 http 모두 처리 가능
 const wss = new WebSocket.Server({ server });
 
+// 어떤 유저들이 지금 소켓에 연결되어 있는지 알기 위해 sockets란 변수에 담아준다. 즉, 연결된 여러 브라우저의 socket값이 담기게 된다.
+const sockets = [];
+
 // 연결 이벤트가 발생했을 때 callback 함수를 실행. 이때 socket이라는 브라우저와 연결해주는 중간자 역할을 하는 인자를 같이 넘겨준다. 즉, socket은 현재 연결된 브라우저를 의미
 wss.on("connection", (socket) => {
+  // 현재 접속한 브라우저의 socket 담기
+  sockets.push(socket);
+  // 닉네임을 정하지 않은 유저는 익명의 유저임을 나타내기
+  socket["nickname"] = "Anonymous";
   // socket안에 내장되어 있는 메서드 send를 통해 메시지를 보낼 수 있다.
   // 이렇게 메시지를 보냈다면 front-end에서도 메시지를 받는 부분을 구현해줘야 한다.
   // 메시지는 frontend에서 event가 된다.
@@ -37,9 +45,29 @@ wss.on("connection", (socket) => {
   // 브라우저에서 전해진 메시지를 감지해서 읽어오기
   socket.on("message", (message) => {
     // 그냥 출력하는 경우 <Buffer eb b8 ... > 이런식으로 깨지는 상황이 생기는데 utf8로 변환해서 출력해주면 이상 없이 값을 확인할 수 있다.
-    console.log(message.toString("utf8"));
+    // console.log(message.toString("utf8"));
     // 서버에서 메시지를 받으면 이를 브라우저에 다시 보내서 작동이 잘 되는지 체크
-    socket.send(message.toString("utf8"));
+    // socket.send(message.toString("utf8"))
+
+    // 현재 연결된 다른 브라우저에도 메시지를 보내서 서로가 주고받는 것처럼 보일 수 있도록 구현
+    // 메시지 문자열로 받아서 그 부분을 다시 객체로 파싱하기
+    const parsedMessage = JSON.parse(message);
+    // console.log(parsed, message.toString("utf8"));
+    // if (parsed.type === "message") {
+    //   sockets.forEach((aSocket) => aSocket.send(parsed.payload));
+    // } else if (parsed.type === "nickname") {
+    //   console.log(parsed.payload);
+    // }
+
+    // switch문으로 전달된 메시지의 타입별로 message인 경우 메시지를 보여주고 닉네임인 경우 socket object안에 nickname이라는 값을 넣어서 활용할 수 있도록 처리
+    switch (parsedMessage.type) {
+      case "message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname} : ${parsedMessage.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = parsedMessage.payload;
+    }
   });
   // socket.send("안녕하세요~ 브라우저로 방문해주신 여러분!!!");
 });
